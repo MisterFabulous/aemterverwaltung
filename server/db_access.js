7ce7023d301579582643b9a0f86446abd98024df
Fabian Böller <fabian@vps281026.ovh.net>
2016-05-23 20:50:28 +0200

    Added 200 http-response when everything is ok

--- server/db_access.js
+++ server/db_access.js
@@ -2,9 +2,12 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var config = require('../config');
 
+var connect = handler => 
+    MongoClient.connect(config().connection.mongo, handler);
+
 // Returns a function, that answers a request data and filters with the query in the url
 exports.get = (table) => (request, response) => {
-    MongoClient.connect(config().connection.mongo, (err, db) => {
+    connect((err, db) => {
 	assert.equal(null, err);
 	console.log("Start querying '" + table + "' data");
 	db.collection(table)
@@ -19,44 +22,41 @@ exports.get = (table) => (request, response) => {
 };
 
 exports.updateBelegungen = (request, response) => {
-    MongoClient.connect(config().connection.mongo, (err, db) => {
+    connect((err, db) => {
 	assert.equal(null, err);
 	console.log("Start updating 'belegungen'");
-	try {
-	    console.log("Start removing old ones");
-	    db.collection('belegungen')
-		.deleteMany({
-		    semester: request.body.semester,
-		    amt: request.body.amt,
-		    org: request.body.org
-		}, (err1, r1) => {
-		    assert.equal(null, err1);
-		    console.log("Removed " + r1.deletedCount + " from 'belegungen'");
-		    console.log(JSON.stringify(request.body));
-		    if(request.body.persons) {
-			console.log("Start inserting new ones");
-			db.collection('belegungen')
-			    .insertMany(request.body.persons.map(p => { return {
-				firstname: p.firstname,
-				lastname: p.lastname,
-				amt: request.body.amt,
-				org: request.body.org,
-				semester: request.body.semester
-			    };}), (err2, r2) => {
-				assert.equal(null, err2);
-				console.log("Inserted " + r2.insertedCount + " into 'belegungen'");
-			    });
-		    }
-		});
-	} catch (e) {
-	    console.log(e);
-	    response.json(e);
-	}
+	console.log("Start removing old ones");
+	db.collection('belegungen')
+	    .deleteMany({
+		semester: request.body.semester,
+		amt: request.body.amt,
+		org: request.body.org
+	    }, (err1, r1) => {
+		assert.equal(null, err1);
+		console.log("Removed " + r1.deletedCount + " from 'belegungen'");
+		console.log(JSON.stringify(request.body));
+		if(request.body.persons) {
+		    console.log("Start inserting new ones");
+		    db.collection('belegungen')
+			.insertMany(request.body.persons.map(p => { return {
+			    firstname: p.firstname,
+			    lastname: p.lastname,
+			    amt: request.body.amt,
+			    org: request.body.org,
+			    semester: request.body.semester
+			};}), (err2, r2) => {
+			    assert.equal(null, err2);
+			    console.log("Inserted " + r2.insertedCount + " into 'belegungen'");
+			    db.close();
+			    response.send(200);
+			});
+		}
+	    });
     });
 };
 
 exports.addPerson = (request, response) => {
-    MongoClient.connect(config().connection.mongo, (err, db) => {
+    connect((err, db) => {
 	assert.equal(null, err);
 	console.log("Start adding 'person'");
 	db.collection('persons')
@@ -68,12 +68,14 @@ exports.addPerson = (request, response) => {
 	    }, (err1, r1) => {
 		assert.equal(1, r1.insertedCount);
 		console.log("Inserted one person");
+		db.close();
+		response.send(200);
 	    });
     });
 };
 
 exports.availableSemesters = (request, response) => {
-    MongoClient.connect(config().connection.mongo, (err, db) => {
+    connect((err, db) => {
 	assert.equal(null, err);
 	console.log("Start collecting available semesters");
 	db.collection("belegungen")
